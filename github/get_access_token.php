d509e02bdba77c8d44467a857f271d2a99b31e23
Assaf Shomer <assafshomer@gmail.com>
2015-10-08 16:32:18 +0300

    working towards access tokens

--- github/get_access_token.php
+++ github/get_access_token.php
@@ -2,9 +2,10 @@
 
 include 'vars.php';
 include SECRET_FILE;
-include ROOT.'/shared/errors.php';
+// include ROOT.'/shared/errors.php';
 
-get_access_token();
+// get_access_token();
+get_basic_auth();
 
 function get_access_token(){
 	$endpoint = '/authorizations/clients/'.GITHUB_CLIENT_ID;
@@ -23,5 +24,42 @@ function get_access_token(){
 	echo $retrievedhtml;
 	return $retrievedhtml;		
 };
+
+function get_auth(){
+	$endpoint = '/authorizations';
+	$formed_url = HOST.$endpoint;
+	$headers = array( 
+		"GET ".$endpoint." HTTP/1.1", 
+		"Host: ".HOST, 
+		"User-Agent: Colu Asset Verificator"
+	);	
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_URL,$formed_url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	$retrievedhtml = curl_exec ($ch);
+	curl_close($ch); 
+	echo $retrievedhtml;
+	return $retrievedhtml;			
+}
+
+function get_basic_auth(){
+	$endpoint = '/user';
+	$formed_url = HOST.$endpoint;
+	$headers = array( 
+		"GET ".$endpoint." HTTP/1.1", 
+		"Host: ".HOST, 
+		"User-Agent: Colu Asset Verificator"
+	);	
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_URL,$formed_url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_USERAGENT, GITHUB_USERNAME);
+	$retrievedhtml = curl_exec ($ch);
+	curl_close($ch); 
+	echo $retrievedhtml;
+	return $retrievedhtml;			
+}
 
 ?>
