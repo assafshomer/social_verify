6fd9985c9edc8207c77b1b53623c25a70bbbd0e1
Assaf Shomer <assafshomer@gmail.com>
2015-10-11 09:54:29 +0300

    removing secret file from index

--- github/github_app_secrets.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('GITHUB_CLIENT_ID', '58f7f1dcbf30eb46a32a');
-define('GITHUB_CLIENT_SECRET', 'a0dd51b0ba8c89f9c2dab479979e8924d6a92c75');
-define('GITHUB_PERSONAL_TOKEN', 'ed5973263e05eb42c6ccaf550ad320583304f430');
-define('GITHUB_USERNAME', 'coloredcoinsassetverification');
-  
-?>
