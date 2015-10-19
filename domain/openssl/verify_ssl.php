817b97f206366cf47ceb590379b2797f71ff85f8
Assaf Shomer <assafshomer@gmail.com>
2015-10-19 16:00:11 +0300

    Verify ssl working when I run it locally, but now when called

--- domain/openssl/verify_ssl.php
+++ domain/openssl/verify_ssl.php
@@ -1,4 +1,27 @@
 <?php
+		$embedded_url = 'https://www.bankofamerica.com';
+
+		$certificate_chain_length = load_certificate_chain($embedded_url);
+		$result_array = get_chain_verification_results($certificate_chain_length);
+		$verification_result = verify_chain($result_array)?"PASS\n":"FAIL\n";
+		$result = get_company_data();
+		$result['verification_result']=$verification_result;
+		$url_matching = ($result['company_url'] == get_domain_from_url($embedded_url))?'TRUE':'false';		
+		$result['url_matching']= $url_matching;
+		var_dump($result);
+
+	function verify_domain($verification_json){
+		$embedded_url = get_url($verifications_json);
+		$certificate_chain_length = load_certificate_chain($embedded_url);
+		$result_array = get_chain_verification_results($certificate_chain_length);
+		$verification_result = verify_chain($result_array)?"PASS\n":"FAIL\n";
+		$result = get_company_data();
+		$result['verification_result']=$verification_result;
+		$url_matching = ($result['company_url'] == get_domain_from_url($embedded_url))?'TRUE':'false';		
+		$result['url_matching']= $url_matching;
+		return $result;
+	};
+
 
 	function verify_chain($array){
 		$array=array_unique($array);
@@ -9,4 +32,44 @@
 		};	
 	};
 
+	function get_chain_verification_results($chain_length){
+		$result_array = array();	
+		for ($x = 0; $x < $chain_length; $x++) {		
+			$result=file_get_contents('tmp/result'.$x.'.txt');
+			preg_match("/0x\S+\s(\w+)\n/", $result,$matches);
+			array_push($result_array,$matches[1]);
+		}
+		return $result_array;
+	};
+
+	function get_company_data(){
+		$subject=exec('./get_company_data.sh');
+		preg_match("/O=(.+)\//U",$subject,$matches);
+		$company_name=$matches[1];
+		preg_match("/CN=(.+)/",$subject,$matches);
+		$company_url=$matches[1];
+		$data = ['company_name' => $company_name,
+						'company_url' => $company_url,];
+		return $data;
+	};
+
+	function load_certificate_chain($url){
+		return exec('./ocsp_load.sh '.$url);
+	};
+
+	function get_url($json){
+		$tmp = json_decode($json,TRUE);
+		$error_message = $tmp['errors'][0]['message'];
+		if (strlen($error_message)>0) {
+			return $error_message;
+		} else {
+			return $tmp['domain']['url'];
+		};
+	};
+
+	function get_domain_from_url($url){
+		preg_match("/https:\/\/(.+)/", $url,$matches);
+		return $matches[1];
+	}
+	
 ?>
