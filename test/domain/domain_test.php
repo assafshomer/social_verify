e4880a7d116883880679b9b1dee1630d250843d7
Assaf Shomer <assafshomer@gmail.com>
2015-10-20 13:33:29 +0300

    grouped tests

--- test/domain/domain_test.php
+++ test/domain/domain_test.php
@@ -9,83 +9,80 @@ $wf_json = load_json('wf');
 $github_json = load_json('github');
 $coinbase_json = load_json('coinbase');
 $colu_json = load_json('colu');
+$bcinfo_json = load_json('blockchaininfo');
 
 // VARS
 $good_array=array('good','good','good');
 $bad_array=array('good','good','bad');
-// $chain_length = load_certificate_chain($bofa_url);
-
-// BANK OF AMERICA
 $bofa_url = 'https://www.bankofamerica.com';
-// $bofa=verify_domain($bofa_url);
-$bofa = verify_domain_json($bofa_json);
-
-// WELLS FARGO
-$wf_url = 'https://www.wellsfargo.com';
-$wf=verify_domain_json($wf_json);
-
-// GITHUB
-$github_url = 'https://github.com';
-$github=verify_domain_json($github_json);
-
-// COINBASE
-$coinbase_url = 'https://www.coinbase.com';
-$coinbase=verify_domain_json($coinbase_json);
-
-// COLU
-$colu_url = 'https://www.colu.co';
-$colu=verify_domain_json($colu_json);
 
 // TESTS
 // $var_test = ()? PASS:FAIL;
 
 $array_test=(verify_chain($good_array) == 1 && verify_chain($bad_array) != 1) ? PASS:FAIL;
-// $load_cert_test = ($chain_length > 0) ? PASS:FAIL;
-$get_url_test = (get_url($bofa_json)==$bofa_url)?PASS:get_url($bofa_json);
-$get_domain_from_url_test = (get_domain_from_url($bofa_url) == 'www.bankofamerica.com' )?PASS:FAIL;
+echo "<br/>array_test: [".$array_test."]";
 
+$get_url_test = (get_url($bofa_json)==$bofa_url)?PASS:get_url($bofa_json);
+echo "<br/>get_url_test: [".$get_url_test."]";
+
+$get_domain_from_url_test = (get_domain_from_url($bofa_url) == 'www.bankofamerica.com' )?PASS:FAIL;
+echo "<br/>get_domain_from_url_test: [".$get_domain_from_url_test."]";
+
+// BANK OF AMERICA
+$bofa = verify_domain_json($bofa_json);
 $bofa_test = ($bofa["company_name"]== "Bank of America Corporation" 
 	&&	$bofa["company_url"]=='www.bankofamerica.com'
 	&&	$bofa["verification_result"]=='PASS'
 	// &&	$bofa["url_matching"]=='TRUE'
 )?PASS:FAIL;
+echo "<br/>bofa_test: [".$bofa_test."]";
 
+// WELLS FARGO
+$wf=verify_domain_json($wf_json);
 $wf_test = ($wf["company_name"]== "Wells Fargo and Company" 
 	&&	$wf["company_url"]=='www.wellsfargo.com'
 	&&	$wf["verification_result"]=='PASS'
 	// &&	$bofa["url_matching"]=='TRUE'
 )?PASS:FAIL;
+echo "<br/>wf_test: [".$wf_test."]";
 
+// GITHUB
+$github=verify_domain_json($github_json);
 $github_test = ($github["company_name"]== "GitHub, Inc." 
 	&&	$github["company_url"]=='github.com'
 	&&	$github["verification_result"]=='PASS'
 	// &&	$bofa["url_matching"]=='TRUE'
 )?PASS:FAIL;
+echo "<br/>github_test: [".$github_test."]";
 
-
+// COINBASE
+$coinbase=verify_domain_json($coinbase_json);
 $coinbase_test = ($coinbase["company_name"]== "Coinbase, Inc." 
 	&&	$coinbase["company_url"]=='www.coinbase.com'
 	&&	$coinbase["verification_result"]=='PASS'
 	// &&	$bofa["url_matching"]=='TRUE'
 )?PASS:var_dump($coinbase);
+echo "<br/>coinbase_test: [".$coinbase_test."]";
 
+// COLU
+$colu=verify_domain_json($colu_json);
 $colu_test = ($colu["company_name"]== "" 
 	&&	$colu["company_url"]=='*.colu.co'
 	&&	$colu["verification_result"]=='PASS'
 	// &&	$bofa["url_matching"]=='TRUE'
 )?PASS:var_dump($colu);
-
-// OUTPUTS
-
-echo "<br/>array_test: [".$array_test."]";
-// echo "<br/>load_cert_test: [".$load_cert_test."]";
-echo "<br/>get_url_test: [".$get_url_test."]";
-echo "<br/>get_domain_from_url_test: [".$get_domain_from_url_test."]";
-echo "<br/>bofa_test: [".$bofa_test."]";
-echo "<br/>wf_test: [".$wf_test."]";
-echo "<br/>github_test: [".$github_test."]";
-echo "<br/>coinbase_test: [".$coinbase_test."]";
 echo "<br/>colu_test: [".$colu_test."]";
+
+
+// BLOCKCHAIN.INFO
+$bcinfo=verify_domain_json($bcinfo_json);
+$bcinfo_test = ($bcinfo["company_name"]== "Blockchain Luxembourg S.A.R.L" 
+	&&	$bcinfo["company_url"]=='www.blockchain.info'
+	&&	$bcinfo["verification_result"]=='PASS'
+	// &&	$bofa["url_matching"]=='TRUE'
+)?PASS:var_dump($bcinfo);
+echo "<br/>bcinfo_test: [".$bcinfo_test."]";
+
 echo "<hr/>";
 // echo "<br/>output: [".$github."]";
 // var_dump(get_chain_verification_results(1,$github_url));
