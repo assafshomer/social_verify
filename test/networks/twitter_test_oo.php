454b1d62f563f78bd3fd988e12e6c3d88a171c0d
Assaf Shomer <assafshomer@gmail.com>
2015-10-28 13:11:22 +0200

    twitter oo done

--- test/networks/twitter_test_oo.php
+++ test/networks/twitter_test_oo.php
@@ -1,14 +1,27 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'].'/verify/shared/errors.php';
+// include $_SERVER['DOCUMENT_ROOT'].'/verify/shared/errors.php';
 include '../test_helper.php';
 include APP_ROOT.'twitter/verify_tweet_oo.php';
 // mimicking json from eyal
 $verified_json = load_json('verified');
+$verified_2_json = load_json('verified_2');
 $unverified_json = load_json('unverified');
 $fake_networks_json = load_json('fake_networks');
 
 $verified = new TwitterVerifier($verified_json);
 $verified_test = ($verified->verified) ? PASS:FAIL;
 echo "<br/>verified_test: [".$verified_test."]";
+
+$verified_2 = new TwitterVerifier($verified_2_json);
+$verified_2_test = ($verified_2->verified) ? PASS:FAIL;
+echo "<br/>verified_2_test: [".$verified_2_test."]";
+
+$unverified = new TwitterVerifier($unverified_json);
+$unverified_test = (!$unverified->verified) ? PASS:FAIL;
+echo "<br/>unverified_test: [".$unverified_test."]";
+
+$fake = new TwitterVerifier($fake_json);
+$fake_test = (!$fake->verified) ? PASS:FAIL;
+echo "<br/>fake_test: [".$fake_test."]";
 
 ?>
